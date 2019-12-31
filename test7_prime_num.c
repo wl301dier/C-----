26c93d67c36185a50d475d3c3f28d1207de7ce23
wl301dier <43742377+wl301dier@users.noreply.github.com>
2020-01-01 00:49:32 +0800

    Update test7_prime_num.c

--- test7_prime_num.c
+++ test7_prime_num.c
@@ -3,7 +3,7 @@
 */
 #include<stdio.h>
 
-int fun(int m){
+int isPrimeNum(int m){
 	for(int i = 2;i<=m-1;i++){//如果m仅能被1和m自身整除（即，2到m-1都不能被m整除），则m是素数。
 		if(m % i == 0){//发现整除可以马上return，可以判定不是素数
 			return 1;
@@ -12,19 +12,45 @@ int fun(int m){
 	return 0;//运行到这里，说明2到m-1都不能被m整除。即m是素数。
 }
 
-int main(){
-	int m = 17;
-	int n = 5;
-	int i = 0;
+void fun(int m,int n,int xx[]){
+		int i = 0;
+		for(;;){
+			m++;
+			if(isPrimeNum(m)==0){
+				xx[i]=m;
+				i++;
+			}
 
-	while(i<n){
-		m++;
-		if(fun(m)==0){
-			i++;
-			printf("%d ",m);
+			if(i==n){
+				break;
+			}
 		}
+}
+
+void fun1(int m,int n,int xx[]){
+		int i = 0;
+		//for(;;)
+		//while(1)
+		while(m++){
+			if(isPrimeNum(m)==0){
+				xx[i]=m;
+				i++;
+			}
+
+			if(i==n){
+				break;
+			}
+		}
+}
+
+int main(){
+	int m,n,zz[1000];
+	printf("\nPlease enter two integers:");
+	scanf("%d %d",&m,&n);
+	fun(m,n,zz);
+	for(m=0;m<n;m++){
+		printf("%d ",zz[m]);
 	}
 	printf("\n");
 	return 0;
-	
 }
